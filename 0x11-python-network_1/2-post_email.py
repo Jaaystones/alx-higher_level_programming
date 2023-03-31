7fc817025fcc8da3092eda37ae06e17ef2336a11
Jaaystones <Joelpau345@gmail.com>
2023-03-31 06:51:36 -0700

    networks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,10 +7,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         html = response.read().decode('utf-8')
         print(html)
-
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Sends a POST request to a given URL with a 
+given email - Displays the body of the response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    response = requests.post(url, data={'email': email})
+    print(response.text)
