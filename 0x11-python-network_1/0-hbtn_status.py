56eed226c3a38f43ee6cb23af95dc464837b532b
Jay stones <Joelpaul345@gmail.com>
2023-03-31 13:59:52 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,11 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-
-print("- type: {}".format(type(html)))
-print("- content: {}".format(html))
-print("- utf8 content: {}".format(html.decode('utf-8')))
-
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
