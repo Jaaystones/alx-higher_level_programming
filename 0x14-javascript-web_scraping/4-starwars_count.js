d1ca13f69245ed62479f103386d801a644225595
Jay stones <Joelpaul345@gmail.com>
2023-04-25 13:28:06 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-const characterId = 18;
-
-request(apiUrl, function (error, response, body) {
-  if (error) throw error;
-
-  const films = JSON.parse(body).results;
-
-  const numMoviesWithWedgeAntilles = films.reduce((acc, film) => {
-    const hasWedgeAntilles = film.characters.some((characterUrl) => {
-      const characterIdMatches = characterUrl.match(/\/(\d+)\/$/);
-      return characterIdMatches && characterIdMatches[1] === characterId.toString();
-    });
-    return acc + hasWedgeAntilles;
-  }, 0);
-
-  console.log(`Number of movies with Wedge Antilles: ${numMoviesWithWedgeAntilles}`);
+const url = process.argv[2];
+let count = 0;
+request(url, function (err, resp, body) {
+  // console.log(resp.statusCode)
+  if (resp.statusCode === 200) {
+    for (const result of JSON.parse(body).results) {
+      for (const character of result.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+  } else {
+    console.error(err);
+  }
+  console.log(count);
 });
