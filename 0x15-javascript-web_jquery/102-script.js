9f9ff610125b3dc267744f03034e339ca5f7e1fc
Jay stones <Joelpaul345@gmail.com>
2023-05-04 00:53:40 +0100

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,9 +1,13 @@
-$(document).ready(function () {
-  $('#btn_translate').click(function () {
-    const langCode = $('#language_code').val();
-    const apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/';
-    $.getJSON(apiUrl, { lang: langCode }, function (data) {
-      $('#hello').text(data.hello);
+(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    $('DIV#hello').empty();
+    const len = $('INPUT#language_code').val();
+    $.ajax({
+      type: 'GET',
+      url: 'https://fourtonfish.com/hellosalut/?lang=' + len,
+      success: function (data) {
+        $('DIV#hello').append(data.hello);
+      }
     });
   });
 });
